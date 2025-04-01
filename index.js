fdc1a9ff3d1016d2471123010db0ce8c0167989a
Rubil <mogererubil@gmail.com>
2025-04-01 13:15:47 +0300

    new commit

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Your code here
+class Cat {
+    constructor(name, gender) {
+        this.name = name;
+        this.gender = gender;
+        this.species = "Cat";
+    }
+
+    speak() {
+        return `${this.name} says meow!`;
+    }
+}
+
+class Dog {
+    constructor(name, gender) {
+        this.name = name;
+        this.gender = gender;
+        this.species = "Dog";
+    }
+
+    speak() {
+        return `${this.name} says woof!`;
+    }
+}
+
+class Bird {
+    constructor(name, gender) {
+        this.name = name;
+        this.gender = gender;
+        this.species = "Bird";
+    }
+
+    speak() {
+        return this.gender === "male"
+            ? `It's me! ${this.name}, the parrot!`
+            : `${this.name} says squawk!`;
+    }
+}
+
+module.exports = { Cat, Dog, Bird };
